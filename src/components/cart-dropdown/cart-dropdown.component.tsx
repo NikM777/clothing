3190c9f0ca107305390388eafb54becfc6b8a009
Nikolay <mushk111@yandex.ru>
2023-07-17 13:48:02 +0800

    Enable closing of CartDropdown component when user clicks outside of it

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,7 +1,12 @@
+import { useEffect, useRef } from "react";
+
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+
+import { useSelector, useDispatch } from "react-redux";
 
 import { selectCartItems } from "../../store/cart/cart.selector";
+import { setIsCartOpen } from "../../store/cart/cart.slice";
+
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
@@ -13,14 +18,35 @@ import {
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
+
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const cartDropdownRef = useRef<HTMLDivElement | null>(null);
 
   const goToCheckoutHandler = () => {
     navigate("/checkout");
   };
 
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        cartDropdownRef.current &&
+        !cartDropdownRef.current.contains(e.target as Node)
+      ) {
+        dispatch(setIsCartOpen(false));
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [cartDropdownRef, dispatch]);
+
   return (
-    <CartDropdownContainer>
+    <CartDropdownContainer ref={cartDropdownRef}>
       <CartItems>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
